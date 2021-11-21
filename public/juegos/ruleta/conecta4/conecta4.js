799a4d03c8c5e28b524265f4f491151e0fa33cd1
DnlCt <a329701@uach.mx>
2021-11-21 06:03:33 +0000

    Se agregó la barra de navegación correcta.

--- public/juegos/ruleta/conecta4/conecta4.js
+++ public/juegos/ruleta/conecta4/conecta4.js
@@ -1,3 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let user = sessionStorage.getItem('username');
+    let wongbucksAmount = sessionStorage.getItem('wongbucks');
+  
+    let username = document.querySelector('#usernameLabel');
+    let wongbucks = document.querySelector('#wongbucksLabel');
+  
+    username.innerHTML = `Username: ${user}`;
+    wongbucks.innerHTML = `Wongbucks: $${wongbucksAmount}`;
+});
+
 while (!player1){
     var player1 = prompt('Player One: Enter your name. You will be red.');
 };
